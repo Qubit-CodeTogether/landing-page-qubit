93457ed372afebe0266858cf1d70649c33a5e81e
danielaryass <daniel.aryass7@gmail.com>
2024-02-20 00:24:09 +0700

    style: change padding

--- containers/project-section/index.tsx
+++ containers/project-section/index.tsx
@@ -8,7 +8,7 @@ import xiyu from "@/public/images/xiyu.jpg";
 
 const ProjectSection = () => {
   return (
-    <div className="p-10S flex flex-col gap-20">
+    <div className="p-10 flex flex-col gap-20">
       <div className="space-y-8 text-center flex flex-col items-center">
         <p className="font-medium text-xl">{`< Projects >`}</p>
         <h2 className="max-w-[466px] text-[40px] font-semibold">
