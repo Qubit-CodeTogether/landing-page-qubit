993f735efd5eff08a4881ed742f9860b18d09b11
danielaryass <daniel.aryass7@gmail.com>
2024-02-20 20:21:59 +0700

    style: change font-size in project section

--- containers/project-section/index.tsx
+++ containers/project-section/index.tsx
@@ -14,7 +14,7 @@ const ProjectSection = () => {
     <div className="p-10 flex flex-col gap-20">
       <div className="space-y-8 text-center flex flex-col items-center">
         <p className="font-medium text-xl">{`< Projects >`}</p>
-        <h2 className="max-w-[466px] text-[40px] font-semibold">
+        <h2 className="max-w-[466px] text-[32px] font-semibold">
           Building innovation is our passion
         </h2>
         <button className="px-5 py-4 bg-black text-white text-xl font-medium rounded-full">
@@ -24,9 +24,8 @@ const ProjectSection = () => {
       <div className="grid grid-cols-2 gap-x-20 gap-y-10">
         <div className="space-y-8">
           <button
-            onClick={() => router.push("/mobile")}
-            className="block w-full"
-          >
+            onClick={() => router.push("/")}
+            className="block w-full">
             {" "}
             <div className="bg-red-200 h-[440px] relative rounded-[32px] overflow-hidden">
               <Image
@@ -38,24 +37,24 @@ const ProjectSection = () => {
               />
               <div className="w-full flex gap-5 px-10 pb-10 absolute bottom-0 left-0 right-0">
                 <Link
-                  href={"mobile"}
-                  className="px-4 py-2  bg-white font-light text-xl rounded-full"
-                >
+                  href={"/"}
+                  className="px-4 py-2  bg-white font-light text-xl rounded-full">
                   Mobile App
                 </Link>
               </div>
             </div>
           </button>
 
-          <Link href={"mobile"} className="font-medium text-[40px] block">
+          <Link
+            href={"/"}
+            className="font-medium text-[32px] block">
             Byte - Social Media App{" "}
           </Link>
         </div>
         <div className="space-y-8">
           <button
-            onClick={() => router.push("/mobile")}
-            className="block w-full"
-          >
+            onClick={() => router.push("/")}
+            className="block w-full">
             {" "}
             <div className="bg-red-200 h-[440px] relative rounded-[32px] overflow-hidden">
               <Image
@@ -67,30 +66,29 @@ const ProjectSection = () => {
               />
               <div className="w-full flex gap-5 px-10 pb-10 absolute bottom-0 left-0 right-0">
                 <Link
-                  href={"mobile"}
-                  className="px-4 py-2  bg-white font-light text-xl rounded-full"
-                >
+                  href={"/"}
+                  className="px-4 py-2  bg-white font-light text-xl rounded-full">
                   Mobile App
                 </Link>
                 <Link
-                  href={"mobile"}
-                  className="px-4 py-2  bg-white font-light text-xl rounded-full"
-                >
+                  href={"/"}
+                  className="px-4 py-2  bg-white font-light text-xl rounded-full">
                   UI/UX
                 </Link>
               </div>
             </div>
           </button>
 
-          <Link href={"mobile"} className="font-medium text-[40px] block">
+          <Link
+            href={"/"}
+            className="font-medium text-[32px] block">
             New Times - News Mobile App
           </Link>
         </div>
         <div className="space-y-8">
           <button
-            onClick={() => router.push("/mobile")}
-            className="block w-full"
-          >
+            onClick={() => router.push("/")}
+            className="block w-full">
             {" "}
             <div className="bg-red-200 h-[440px] relative rounded-[32px] overflow-hidden">
               <Image
@@ -102,24 +100,24 @@ const ProjectSection = () => {
               />
               <div className="w-full flex gap-5 px-10 pb-10 absolute bottom-0 left-0 right-0">
                 <Link
-                  href={"mobile"}
-                  className="px-4 py-2  bg-white font-light text-xl rounded-full"
-                >
+                  href={"/"}
+                  className="px-4 py-2  bg-white font-light text-xl rounded-full">
                   Website
                 </Link>
               </div>
             </div>
           </button>
 
-          <Link href={"mobile"} className="font-medium text-[40px] block">
+          <Link
+            href={"/"}
+            className="font-medium text-[32px] block">
             Gemini - Company Profile
           </Link>
         </div>
         <div className="space-y-8">
           <button
-            onClick={() => router.push("/mobile")}
-            className="block w-full"
-          >
+            onClick={() => router.push("/")}
+            className="block w-full">
             {" "}
             <div className="bg-red-200 h-[440px] relative rounded-[32px] overflow-hidden">
               <Image
@@ -132,14 +130,15 @@ const ProjectSection = () => {
               <div className="w-full flex gap-5 px-10 pb-10 absolute bottom-0 left-0 right-0">
                 <Link
                   href={"XIyu"}
-                  className="px-4 py-2  bg-white font-light text-xl rounded-full"
-                >
+                  className="px-4 py-2  bg-white font-light text-xl rounded-full">
                   Mobile App
                 </Link>
               </div>
             </div>
           </button>
-          <Link href={"mobile"} className="font-medium text-[40px] block">
+          <Link
+            href={"/"}
+            className="font-medium text-[32px] block">
             XiYu - Health Mobile App
           </Link>
         </div>
